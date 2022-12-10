d860c3c636480b771196269c472d7980fb687b3b
naufalyesha <naufalyesha2@gmail.com>
2022-12-10 20:29:56 +0700

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json

---
+++ src/pages/Login/LoginScreen.js
@@ -0,0 +1,53 @@
+import { useAuth } from "../../../contexts/AuthContext";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, TextInput, Button, View } from "react-native";
+
+const LoginScreen = ({ navigation }) => {
+    const { login } = useAuth();
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                value={user}
+                onChangeText={(text) => setUser(text)}
+                placeholder={"Username"}
+                style={styles.input}
+            />
+            <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                placeholder={"Password"}
+                secureTextEntry={true}
+                style={styles.input}
+            />
+
+            <Button
+                title={"Login"}
+                style={styles.input}
+                onPress={() => login(user, password)}
+            />
+        </View>
+    );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ecf0f1",
+    },
+    input: {
+        width: 200,
+        height: 44,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        marginBottom: 10,
+    },
+});
---
+++ src/styles/color.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    homeBackgroud: '#E8E8E8',
+    noteBackground: '#fff',
+    notes:'#fff',
+    header: '#0456FA',
+    headerButtons:'#000',
+    addButton:'#0456FA',
+    loading:'#0000ff',
+    searchBar:'#d3d3d3'
+}
+
+export default COLORS;
